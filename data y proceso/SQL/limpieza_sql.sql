3afcee42fec6c934689221dcee6b351ea2f87846
lau-ont <laura.onteniente@gmail.com>
2022-11-28 09:04:17 +0100

    Actualización de ficheros

---
+++ data y proceso/SQL/limpieza_sql.sql
@@ -0,0 +1,21 @@
+USE proyecto_1_equipo_1;
+
+UPDATE data_sql SET  q10_part_1= "NULL" WHERE q10_part_1= "ERROR";
+UPDATE data_sql SET  q10_part_2= "NULL" WHERE q10_part_2= "ERROR";
+UPDATE data_sql SET  q10_part_3= "NULL" WHERE q10_part_3= "ERROR";
+UPDATE data_sql SET  q10_part_4= "NULL" WHERE q10_part_4= "ERROR";
+UPDATE data_sql SET  q10_part_5= "NULL" WHERE q10_part_5= "ERROR";
+UPDATE data_sql SET  q10_part_6= "NULL" WHERE q10_part_6= "ERROR";
+UPDATE data_sql SET  q10_part_7= "NULL" WHERE q10_part_7= "ERROR";
+UPDATE data_sql SET  q10_part_8= "NULL" WHERE q10_part_8= "ERROR";
+UPDATE data_sql SET  q10_part_9= "NULL" WHERE q10_part_9= "ERROR";
+UPDATE data_sql SET  q10_part_10= "NULL" WHERE q10_part_10= "ERROR";
+UPDATE data_sql SET  q10_part_11= "NULL" WHERE q10_part_11= "ERROR";
+UPDATE data_sql SET  q10_part_12= "NULL" WHERE q10_part_12= "ERROR";
+UPDATE data_sql SET  q10_part_13= "NULL" WHERE q10_part_13= "ERROR";
+UPDATE data_sql SET  q10_part_14= "NULL" WHERE q10_part_14= "ERROR";
+UPDATE data_sql SET  q10_part_15= "NULL" WHERE q10_part_15= "ERROR";
+UPDATE data_sql SET  q10_part_16= "NULL" WHERE q10_part_16= "ERROR";
+UPDATE data_sql SET q10_other= "NULL" WHERE q10_other= "ERROR";
+
+
